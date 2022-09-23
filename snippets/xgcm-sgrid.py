e0c764555fcb75acf15d57c6aeb8ae8a5aae1651
Ben Orchard <thefirstmuffinman@gmail.com>
2022-09-23 15:37:48 +0100

    add wip notes/Python snippets for SGRID parsing

---
+++ snippets/xgcm-sgrid.py
@@ -0,0 +1,26 @@
+import xarray as xr
+
+def extract_xgcm_meta_from_sgrid_data(ds):
+    """
+    Analyse an xarray Dataset using SGRID conventions
+    """
+
+
+
+# comments are approximations to the original SGRID concept
+
+    { "X": # XZ coord?
+        { "center": "MC" # SGRID node
+        , "left":   "M"  # SGRID face
+        }
+} , { "Y": # YZ coord?
+        { "center": "NC" # SGRID node
+        , "left":   "N"  # SGRID face
+        }
+  }
+
+"""
+* inspect ds.grid
+  * inspect face_dimensions
+  *
+"""
